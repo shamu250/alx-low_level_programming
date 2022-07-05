34458282e8b38af7a500f1c21daf45f5d4fd1477
shamu <muhozashamu@gmail.com>
2022-07-05 11:57:53 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers of base 10
+ *
+ * Description: using the main function
+ * this program prints "single digit numbers of base 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c<= '9'; c++)
+{
+	putchar(c);
+}
+putchar('\n')
+return (0);
+}
