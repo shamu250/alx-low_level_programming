aaa07c0d1b130e88863b04b418ab82ac6ebca5c0
shamu <muhozashamu@gmail.com>
2022-07-05 09:27:53 -0700

    Task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main
+ * this program prints "the size of various typeson computer it is compiled
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
