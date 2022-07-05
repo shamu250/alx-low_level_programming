2ef824e6f2350ef996ce03f657066303231160f6
shamu <muhozashamu@gmail.com>
2022-07-05 12:41:59 -0700

    Task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int c;
-	for (c = 48; c <= 57; c++)
+int c;
+for (c = 48; c <= 57; c++)
+{
+	putchar(c);
+	if (c != 57)
 	{
-		putchar(c);
-		if (c != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
