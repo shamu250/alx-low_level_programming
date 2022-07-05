be8aa071a967f257f1732fa9d791acd38354d5d5
shamu <muhozashamu@gmail.com>
2022-07-05 11:41:11 -0700

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printthe alphabet in lowercase
+ *
+ * Description: using the main function
+ * this program prints "the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q' )
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
