6c8bc44e4ebdd9b993a8a86f86209bb480a99fe6
shamu <muhozashamu@gmail.com>
2022-07-08 10:23:04 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'}
+	char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
 	for (c = 0; c < sizeof(_putchar); c++)
