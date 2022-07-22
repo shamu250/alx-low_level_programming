8727e6af10591dbb3fe0442a16f1c17c996ad573
shamu <muhozashamu@gmail.com>
2022-07-22 05:53:29 -0700

    Task100

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,8 @@ int help(int c, int i);
 int is_prime_number(int n);
 int hei(int n, int d);
 int _sqrt(int x, int i);
+int is_palindrome(char *s);
+int get_length(char *s);
+int my_pal(char *s, int l);
+int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
